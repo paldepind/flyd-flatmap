28db3343cc85d51ffdee1cf0293aba687f8047e4
paldepind <simonfv@gmail.com>
2015-03-19 16:43:13 +0100

    More compact implementation

--- flatmap.js
+++ flatmap.js
@@ -1,9 +1,7 @@
 var flyd = require('flyd');
 
 module.exports = function(s, f) {
-  var ns = flyd.stream();
-  flyd.map(s, function(v) {
-    flyd.map(f(v), ns);
+  return flyd.stream([s], function(own) {
+    flyd.map(f(s.val), own);
   });
-  return ns;
 };
